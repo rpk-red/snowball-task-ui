fc4b4deac858a69ac7fd65671541655dd40b5a71
Goran Ribic <gr.posao@gmail.com>
2020-03-30 05:42:21 +0200

    Updates

---
+++ precache-manifest.90c0791e95be597b0e04397d2920c4ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a71b01e389d553c8fda40cb5459207",
+    "url": "/snowball-task-ui/index.html"
+  },
+  {
+    "revision": "91e50415930aed1838fd",
+    "url": "/snowball-task-ui/static/css/main.0a3bb64c.chunk.css"
+  },
+  {
+    "revision": "03e276ea46743e614025",
+    "url": "/snowball-task-ui/static/js/2.e057ae8d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/snowball-task-ui/static/js/2.e057ae8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91e50415930aed1838fd",
+    "url": "/snowball-task-ui/static/js/main.35029049.chunk.js"
+  },
+  {
+    "revision": "bdc459211be117ef07ea",
+    "url": "/snowball-task-ui/static/js/runtime-main.eaa05d7e.js"
+  }
+]);
